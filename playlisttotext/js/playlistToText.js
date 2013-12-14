4169c6008043a9e0435f1c25e1d3326ab1288f0a
miigwall <miigwall@gmail.com>
2013-12-14 14:04:24 +0200

    Update playlistToText.js

--- playlisttotext/js/playlistToText.js
+++ playlisttotext/js/playlistToText.js
@@ -3,13 +3,14 @@
  * PlaylistToText (ExportToCSV Modification)
  * Original code by Scruteur (http://blog.geted.info/index.php?post/2012/08/10/Export-de-playlist-au-format-CSV-pour-Spotify)
  * Modifications by MiiG (MjK Web Services 2013)
- * Last edit: 23. November 2013
+ * Last edit: 14. December 2013
  */
 $(document).ready(function() {
 	$("#select_all").hide();
 	$("#select_all").click(function() {
 		$("#dropArea").select();
 	});
+	$("#dropArea").css({'background-image': 'url(img/spotex.png)'});
 
 	var sp = getSpotifyApi();
 	var models = sp.require('$api/models');
@@ -99,20 +100,21 @@ $(document).ready(function() {
         drop.addEventListener('drop', function(e) {
 			e.preventDefault();
 			this.classList.remove('over');
-			this.style.background = '#FFF';
+			this.style.background = '#fff';
 			var url = e.dataTransfer.getData('Text');
 			var uri = getPlaylistURI(url);
 			var fieldSep = $('#delimiter').val();
 			var escChar = $('#textQualifier').val();
-			var headerLine = $('#headerLine').val();
-			var listColumn = $('#listColumn').val();
-			var linkColumn = $('#linkColumn').val();
-			var albumColumn = $('#albumColumn').val();
-			var numberColumn = $('#numberColumn').val();
-			var yearColumn = $('#yearColumn').val();
-			var durationColumn = $('#durationColumn').val();
-			var starredColumn = $('#starredColumn').val();
-			var uriColumn = $('#uriColumn').val();
+			var headerLine = $('#headerLine').prop('checked');
+			var infoRow = $('#infoRow').prop('checked');
+			var listColumn = $('#listColumn').prop('checked');
+			var linkColumn = $('#linkColumn').prop('checked');
+			var albumColumn = $('#albumColumn').prop('checked');
+			var numberColumn = $('#numberColumn').prop('checked');
+			var yearColumn = $('#yearColumn').prop('checked');
+			var durationColumn = $('#durationColumn').prop('checked');
+			var starredColumn = $('#starredColumn').prop('checked');
+			var uriColumn = $('#uriColumn').prop('checked');
 			$('#configuration').empty();
 			$('#configuration').append('Options <img src="img/icon_foldMinus.png" />');
 			$('#options_area').hide();
@@ -132,30 +134,33 @@ $(document).ready(function() {
 				// ---- HEADER LINE ----
 				var header = "";
 				
-				if(numberColumn == 'yes') {
+				if(numberColumn == true) {
 					header += getChar(escChar)+"X. "+getChar(escChar)+getChar(fieldSep);
 				}
 				
 				header += getChar(escChar)+"TRACK"+getChar(escChar)+" "+getChar(fieldSep)+" "+getChar(escChar)+"ARTIST"+getChar(escChar);
 							
-				if(albumColumn == 'yes') {
+				if(albumColumn == true) {
 					header += getChar(fieldSep)+getChar(escChar)+"ALBUM"+getChar(escChar);
 				}
-				if(durationColumn == 'yes') {
+				if(durationColumn == true) {
 					header += getChar(fieldSep)+getChar(escChar)+"DURATION"+getChar(escChar);
 				}
-				if(yearColumn == 'yes') {
+				if(yearColumn == true) {
 					header += getChar(fieldSep)+getChar(escChar)+"YEAR"+getChar(escChar);
 				}
-				if(starredColumn == 'yes') {
+				if(starredColumn == true) {
 					header += getChar(fieldSep)+getChar(escChar)+"STAR"+getChar(escChar);
 				}
-				if(uriColumn == 'yes') {
+				if(uriColumn == true) {
 					header += getChar(fieldSep)+getChar(escChar)+"SPOTIFY URL"+getChar(escChar);
 				}
 				header += "\n";
 				
-				if(listColumn == 'yes') {
+				if(infoRow == true) {
+					line += "--- "+pl.name+"&nbsp;("+pl.length+" tracks, "+pl.subscriberCount+" subscribers) --- \n";
+				}			
+				if(listColumn == true) {
 					line += "&lt;ol class=\"spotify_list\"&gt;";
 				}
 				
@@ -177,54 +182,52 @@ $(document).ready(function() {
 					$('#playlistName').append("Current playlist: " + pl.name + "<span id='playlistInfos'>&nbsp;(" + pl.length + " tracks, " + pl.subscriberCount + " subscribers)</span>");
 					
 					// ---- LINE ----
-					if(listColumn == 'yes') {
+					if(listColumn == true) {
 						line += "&lt;li&gt;";
 					}
-					
-					if(numberColumn == 'yes') {
+					if(numberColumn == true) {
 						line += getChar(escChar)+(i+1)+". "+getChar(escChar);
 					}
-					if(linkColumn == 'yes') {
+					if(linkColumn == true) {
 						line += "&lt;a href=\""+tracks[i].uri+"\"&gt;";
 					}
 					
 					line += getChar(escChar)+escapeChar(artistsStr, escChar)+getChar(escChar)+" "+getChar(fieldSep)+" ";
 					line += getChar(escChar)+escapeChar(tracks[i].name, escChar)+getChar(escChar);		
 								
-					if(albumColumn == 'yes') {
+					if(albumColumn == true) {
 						line += getChar(fieldSep);
 						line += getChar(escChar)+escapeChar(tracks[i].album.name, escChar)+getChar(escChar);
 					}
-					if(durationColumn == 'yes') {
+					if(durationColumn == true) {
 						line += getChar(fieldSep);
 						line += getChar(escChar)+tracks[i].duration+getChar(escChar);
 					}
-					if(yearColumn == 'yes') {
+					if(yearColumn == true) {
 						line += getChar(fieldSep);
 						line += getChar(escChar)+tracks[i].album.year+getChar(escChar);
 					}
-					if(starredColumn == 'yes') {
+					if(starredColumn == true) {
 						line += getChar(fieldSep);
 						line += getChar(escChar)+getStarred(tracks[i].starred)+getChar(escChar);
 					}
-					if(uriColumn == 'yes') {
+					if(uriColumn == true) {
 						line += getChar(fieldSep);
 						line += getChar(escChar)+tracks[i].uri+getChar(escChar);
 					}
-					if(linkColumn == 'yes') {
+					if(linkColumn == true) {
 						line += "&lt;/a&gt;";
 					}
-					if(listColumn == 'yes') {
+					if(listColumn == true) {
 						line += "&lt;/li&gt;";
 					} else {
 						line += "\n";
 					}
 				}
-				if(listColumn == 'yes') {
+				if(listColumn == true) {
 					line += "&lt;/ol&gt;";
 				}
-					
-				if(headerLine == 'yes') {
+				if(headerLine == true) {
 					line = header+line;
 				}
 				$('#dropArea').append(line);
